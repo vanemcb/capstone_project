7f1cfeedc6f4ceece193652bacb5a3355b38950d
vanemcb <vanem2201@gmail.com>
2021-10-20 11:57:27 -0500

    database.sql | table init_survey created

--- server/database.sql
+++ server/database.sql
@@ -28,3 +28,26 @@ CREATE TABLE survey (
 	business_field VARCHAR(128),
 	compensation VARCHAR
 	);
+
+CREATE TABLE init_survey (
+	id SERIAL PRIMARY KEY,
+	login_id INT,
+	FOREIGN KEY(login_id)
+	REFERENCES users(login_id),
+	company VARCHAR(128),
+	company_location VARCHAR(128),
+	job_position VARCHAR(128),
+	job_level VARCHAR(128),
+	years_of_experience FLOAT,
+	years_at_company FLOAT,
+	montly_salary INT,
+	currency VARCHAR(32),
+	bonus INT,
+	frequency VARCHAR(32),
+	gender VARCHAR(12)
+	);
+
+CREATE TABLE test (
+	title VARCHAR(255),
+	description VARCHAR(255)
+	);
