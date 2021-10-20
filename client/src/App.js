899e47a05bb8cab9147fc843118dad36ee1a081c
vanemcb <vanem2201@gmail.com>
2021-10-20 11:54:40 -0500

    App | App function created and class APP commented

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
-
-// sample dictionary
-import filled_survey from './Sample/filled_survey.json'
 
 // components
 import Survey from './Components/survey'
 
-class App extends Component {
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <Survey />
+      </div>
+    </Fragment>
+  );
+};
 
-    state = {
-        filled_survey: filled_survey
-    }
+// class App extends Component {
 
-    addSurvey = (new_survey) => {
-        filled_survey.push(new_survey)
-        console.log(new_survey)
-        console.log(filled_survey)
-    }
+//     state = {
+//         filled_survey: filled_survey
+//     }
 
-    render() {
-        return <div>
-            <Survey addSurvey={this.addSurvey} />
-        </div >
-    }
-}
+//     addSurvey = (new_survey) => {
+//         filled_survey.push(new_survey)
+//         console.log(new_survey)
+//         console.log(filled_survey)
+//     }
+
+//     render() {
+//         return <div>
+//             <Survey addSurvey={this.addSurvey} />
+//         </div >
+//     }
+// }
 
 export default App;
