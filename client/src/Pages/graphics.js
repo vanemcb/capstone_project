13789edfd4dce35dea656dcbad6e7336d67bf2cc
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-10-31 21:11:11 -0500

    debugging

--- client/src/Pages/graphics.js
+++ client/src/Pages/graphics.js
@@ -14,52 +14,53 @@ const Graphics = () => {
     const [position, setPosition] = useState("")
 
     return (
-        <Fragment>
-            <Navigation />
-            <Row className="show-grid" style={{ margin: '20px 10px 10px 10px' }}>
-                <Col className="show-grid" xs={2} md={2} style={{ width: 230 }} >
-                    <Row className="mx-auto">
-                        <HomeSideBar />
-                    </Row >
-                    <Row >
-                      <CardCompany dicCompany={dicCompany} setPosition={setPosition} position={position}/>
-                    </Row>
-                </Col>
-                <Col className="show-grid" xs={10} md={10}>
-                    <Row style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        margin: '0px 0px 20px 0px',
-                        alignContent: 'space-between'
-                    }}>
-                      <ToggleCompanies setDicCompany={setDicCompany} />
-                    </Row>
-                    <Row style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        margin: '0px 0px 20px 0px',
-                        alignContent: 'space-between'
-                    }}>
-                        <Col className="show-grid" xs={5} md={5}>
-                            <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
-                              <BarByCompany dicCompany={dicCompany}/>
-                            </Row>
-                        </Col>
-                        <Col className="show-grid" xs={5} md={5}>
-                            <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
-                              <DouByCompany dicCompany={dicCompany}/>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        margin: '0px 0px 20px 0px',
-                        alignContent: 'space-between'
-                    }}>
-                      <CardBenefits dicCompany={dicCompany}/>
-                    </Row>
-                </Col>
+        <div style={{ position: 'relative', minHeight: '100vh' }}>
+            <Fragment>
+                <Navigation />
+                <Row className="show-grid" style={{ margin: '20px 10px 10px 10px' }}>
+                    <Col className="show-grid" xs={2} md={2} style={{ width: 230 }} >
+                        <Row className="mx-auto">
+                            <HomeSideBar />
+                        </Row >
+                        <Row >
+                            <CardCompany dicCompany={dicCompany} setPosition={setPosition} position={position} />
+                        </Row>
+                    </Col>
+                    <Col className="show-grid" xs={10} md={10}>
+                        <Row style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            margin: '0px 0px 20px 0px',
+                            alignContent: 'space-between'
+                        }}>
+                            <ToggleCompanies setDicCompany={setDicCompany} />
+                        </Row>
+                        <Row style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            margin: '0px 0px 20px 0px',
+                            alignContent: 'space-between'
+                        }}>
+                            <Col className="show-grid" xs={5} md={5}>
+                                <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
+                                    <BarByCompany dicCompany={dicCompany} />
+                                </Row>
+                            </Col>
+                            <Col className="show-grid" xs={5} md={5}>
+                                <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
+                                    <DouByCompany dicCompany={dicCompany} />
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            margin: '0px 0px 20px 0px',
+                            alignContent: 'space-between'
+                        }}>
+                            <CardBenefits dicCompany={dicCompany} />
+                        </Row>
+                    </Col>
 
                 </Row>
                 <Row>
