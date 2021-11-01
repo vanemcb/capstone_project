e1a43804f04f8fca0b6391d4e025774b59db76ab
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-11-01 11:46:33 -0500

    solving footer and preparing to merge

--- client/src/Pages/general_info.js
+++ client/src/Pages/general_info.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Navigation from "../Components/navigation";
 import FooterBar from "../Components/footer";
-import HomeSideBar from "../Components/home_sidebar"
+import SurveyButton from "../Components/survey_button"
 import CardCompany from "../Components/CardCompany"
 import ToggleCompanies from "../Components/ToggleCompanies";
 
@@ -11,51 +11,52 @@ const GeneralInfo = () => {
     const [dicCompany, setDicCompany] = useState(null)
     const [company, setCompany] = useState("")
     return (
-        <div style={{ position: 'relative', minHeight: '100vh' }}>
-            <Fragment>
-                <Navigation />
-                <Row className="show-grid" style={{ margin: '20px 10px 10px 10px' }}>
-                    <Col className="show-grid" xs={2} md={2} style={{ width: 230 }} >
-                        <Row className="me-auto" >
-                            <HomeSideBar />
-                        </Row >
-                        <Row >
-                            <CardCompany dicCompany={dicCompany} company={company} />
-                        </Row>
-                    </Col>
-                    <Col className="show-grid" xs={10} md={10}>
-                        <Row style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            margin: '0px 0px 20px 0px',
-                            alignContent: 'space-between'
-                        }}>
-                            <ToggleCompanies setDicCompany={setDicCompany} setCompany={setCompany} />
-                        </Row>
-                        <Row style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            margin: '0px 0px 20px 0px',
-                            alignContent: 'space-between'
-                        }}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius sem dictum nulla blandit molestie. Phasellus finibus dui fringilla, aliquet tortor lacinia, blandit purus. Phasellus risus enim, luctus in aliquet ornare, vulputate non purus. Phasellus eu metus et augue porta mollis vel non lectus. Cras sed tellus felis. Nullam in ante non elit ultricies consectetur vel sed magna. Phasellus condimentum facilisis lectus, vel congue est aliquet ut. Donec vitae sapien massa. Nam sem est, mollis id semper ut, aliquam a metus. Aliquam porttitor bibendum arcu, at suscipit magna ultricies eget. Nulla eu purus id eros auctor eleifend id sit amet metus. Nunc molestie viverra nulla eget pretium. Donec quis tellus a eros rutrum gravida. Nunc congue ante ac odio sodales bibendum. Morbi et tellus nibh. Pellentesque quis semper elit, sit amet vestibulum nunc.
-                        </Row>
-                        <Row style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            margin: '0px 0px 20px 0px',
-                            alignContent: 'space-between'
-                        }}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius sem dictum nulla blandit molestie. Phasellus finibus dui fringilla, aliquet tortor lacinia, blandit purus. Phasellus risus enim, luctus in aliquet ornare, vulputate non purus. Phasellus eu metus et augue porta mollis vel non lectus. Cras sed tellus felis. Nullam in ante non elit ultricies consectetur vel sed magna. Phasellus condimentum facilisis lectus, vel congue est aliquet ut. Donec vitae sapien massa. Nam sem est, mollis id semper ut, aliquam a metus. Aliquam porttitor bibendum arcu, at suscipit magna ultricies eget. Nulla eu purus id eros auctor eleifend id sit amet metus. Nunc molestie viverra nulla eget pretium. Donec quis tellus a eros rutrum gravida. Nunc congue ante ac odio sodales bibendum. Morbi et tellus nibh. Pellentesque quis semper elit, sit amet vestibulum nunc.
-                        </Row>
-                    </Col>
-                </Row>
-                <Row>
-                    <FooterBar />
-                </Row>
-            </Fragment>
-
-        </div >
+        <div>
+            <div style={{ position: 'relative', minHeight: '100vh' }}>
+                <Fragment>
+                    <Navigation />
+                    <Row className="show-grid" style={{ margin: '20px 10px 10px 10px' }}>
+                        <Col className="show-grid" xs={2} md={2} style={{ width: 230 }} >
+                            <Row className="mx-auto" style={{ margin: '10px 0px 30px 0px' }}>
+                                <SurveyButton />
+                            </Row >
+                            <Row className="mx-auto">
+                                <CardCompany dicCompany={dicCompany} company={company} />
+                            </Row>
+                        </Col>
+                        <Col className="show-grid" xs={10} md={10}>
+                            <Row style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                margin: '10px 0px 30px 0px',
+                                alignContent: 'space-between'
+                            }}>
+                                <ToggleCompanies setDicCompany={setDicCompany} setCompany={setCompany} />
+                            </Row>
+                            <Row style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                margin: '0px 0px 20px 0px',
+                                alignContent: 'space-between'
+                            }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius sem dictum nulla blandit molestie. Phasellus finibus dui fringilla, aliquet tortor lacinia, blandit purus. Phasellus risus enim, luctus in aliquet ornare, vulputate non purus. Phasellus eu metus et augue porta mollis vel non lectus. Cras sed tellus felis. Nullam in ante non elit ultricies consectetur vel sed magna. Phasellus condimentum facilisis lectus, vel congue est aliquet ut. Donec vitae sapien massa. Nam sem est, mollis id semper ut, aliquam a metus. Aliquam porttitor bibendum arcu, at suscipit magna ultricies eget. Nulla eu purus id eros auctor eleifend id sit amet metus. Nunc molestie viverra nulla eget pretium. Donec quis tellus a eros rutrum gravida. Nunc congue ante ac odio sodales bibendum. Morbi et tellus nibh. Pellentesque quis semper elit, sit amet vestibulum nunc.
+                            </Row>
+                            <Row style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                margin: '0px 0px 20px 0px',
+                                alignContent: 'space-between'
+                            }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras varius sem dictum nulla blandit molestie. Phasellus finibus dui fringilla, aliquet tortor lacinia, blandit purus. Phasellus risus enim, luctus in aliquet ornare, vulputate non purus. Phasellus eu metus et augue porta mollis vel non lectus. Cras sed tellus felis. Nullam in ante non elit ultricies consectetur vel sed magna. Phasellus condimentum facilisis lectus, vel congue est aliquet ut. Donec vitae sapien massa. Nam sem est, mollis id semper ut, aliquam a metus. Aliquam porttitor bibendum arcu, at suscipit magna ultricies eget. Nulla eu purus id eros auctor eleifend id sit amet metus. Nunc molestie viverra nulla eget pretium. Donec quis tellus a eros rutrum gravida. Nunc congue ante ac odio sodales bibendum. Morbi et tellus nibh. Pellentesque quis semper elit, sit amet vestibulum nunc.
+                            </Row>
+                        </Col>
+                    </Row>
+                </Fragment>
+            </div >
+            <div className='fixed-bottom'>
+                <FooterBar />
+            </div>
+        </div>
     );
 };
 
--- client/src/Pages/graphics.js
+++ client/src/Pages/graphics.js
@@ -4,7 +4,7 @@ import Navigation from "../Components/navigation";
 import FooterBar from "../Components/footer";
 import BarByCompany from "../Components/BarByCompany"
 import DouByCompany from "../Components/DouByCompany"
-import HomeSideBar from "../Components/home_sidebar"
+import SurveyButton from "../Components/survey_button"
 import CardCompany from "../Components/CardCompany";
 import ToggleCompanies from "../Components/ToggleCompanies"
 import CardBenefits from "../Components/CardBenefits";
@@ -14,59 +14,61 @@ const Graphics = () => {
     const [position, setPosition] = useState("")
 
     return (
-        <div style={{ position: 'relative', minHeight: '100vh' }}>
-            <Fragment>
-                <Navigation />
-                <Row className="show-grid" style={{ margin: '20px 10px 10px 10px' }}>
-                    <Col className="show-grid" xs={2} md={2} style={{ width: 230 }} >
-                        <Row className="mx-auto">
-                            <HomeSideBar />
-                        </Row >
-                        <Row >
-                            <CardCompany dicCompany={dicCompany} setPosition={setPosition} position={position} />
-                        </Row>
-                    </Col>
-                    <Col className="show-grid" xs={10} md={10}>
-                        <Row style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            margin: '0px 0px 20px 0px',
-                            alignContent: 'space-between'
-                        }}>
-                            <ToggleCompanies setDicCompany={setDicCompany} />
-                        </Row>
-                        <Row style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            margin: '0px 0px 20px 0px',
-                            alignContent: 'space-between'
-                        }}>
-                            <Col className="show-grid" xs={5} md={5}>
-                                <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
-                                    <BarByCompany dicCompany={dicCompany} />
-                                </Row>
-                            </Col>
-                            <Col className="show-grid" xs={5} md={5}>
-                                <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
-                                    <DouByCompany dicCompany={dicCompany} />
-                                </Row>
-                            </Col>
-                        </Row>
-                        <Row style={{
-                            display: 'flex',
-                            flexWrap: 'wrap',
-                            margin: '0px 0px 20px 0px',
-                            alignContent: 'space-between'
-                        }}>
-                            <CardBenefits dicCompany={dicCompany} />
-                        </Row>
-                    </Col>
+        <div >
+            <div style={{ position: 'relative', minHeight: '100vh' }}>
+                <Fragment >
+                    <Navigation />
+                    <Row className="show-grid" style={{ margin: '20px 10px 10px 10px' }}>
+                        <Col className="show-grid" xs={2} md={2} style={{ width: 230 }} >
+                            <Row className="mx-auto" style={{ margin: '10px 0px 30px 0px' }}>
+                                < SurveyButton />
+                            </Row >
+                            <Row className="mx-auto" >
+                                <CardCompany dicCompany={dicCompany} setPosition={setPosition} position={position} />
+                            </Row>
+                        </Col>
+                        <Col className="show-grid" xs={10} md={10}>
+                            <Row style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                margin: '10px 0px 30px 0px',
+                                alignContent: 'space-between'
+                            }}>
+                                <ToggleCompanies setDicCompany={setDicCompany} />
+                            </Row>
+                            {/* graphics container */}
+                            <Row style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                margin: '0px 0px 20px 0px',
+                                alignContent: 'space-between'
+                            }}>
+                                <Col className="show-grid" xs={6} md={6}>
+                                    <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
+                                        <BarByCompany dicCompany={dicCompany} />
+                                    </Row>
+                                </Col>
+                                <Col className="show-grid" xs={6} md={6}>
+                                    <Row style={{ margin: '0px 20px 10px 0px', display: 'flex' }}>
+                                        <DouByCompany dicCompany={dicCompany} />
+                                    </Row>
+                                </Col>
+                            </Row>
+                            <Row style={{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                alignContent: 'space-between'
+                            }}>
+                                <CardBenefits dicCompany={dicCompany} />
+                            </Row>
+                        </Col>
+                    </Row>
+                </Fragment>
 
-                </Row>
-                <Row>
-                    <FooterBar />
-                </Row>
-            </Fragment>
+            </div>
+            <div>
+                <FooterBar />
+            </div>
         </div>
     );
 };
